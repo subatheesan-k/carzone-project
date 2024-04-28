ab99fcb54c29e9096c9cb1ad6ad86c6cd1b36601
Subathees <dev16.smakintel@gmail.com>
2024-04-28 17:28:23 +0530

    project re created

--- carzone/urls.py
+++ carzone/urls.py
@@ -1,7 +1,8 @@
-"""carzone URL Configuration
+"""
+URL configuration for carzone project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
